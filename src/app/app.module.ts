608bd0419dc2c369c2121d6bf710054185e7c7f4
edmiralagic <edmir.alagic@my.ccsu.edu>
2018-08-14 21:46:02 -0400

    initial frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SplashComponent } from './splash/splash.component';
+import { HowtoComponent } from './howto/howto.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SplashComponent,
+    HowtoComponent,
+    BenefitsComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/howto/howto.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-howto',
+  templateUrl: './howto.component.html',
+  styleUrls: ['./howto.component.css']
+})
+
+export class HowtoComponent implements OnInit {
+  toggleStep(num) {
+    var howto = document.getElementById("howto");
+    var lists = howto.getElementsByTagName("ul");
+    for (var i = 0; i < lists.length; ++i) {
+      var items = lists[i].getElementsByTagName("li");
+      for (var j = 0; j < items.length; ++j) {
+        items[j].classList.remove("is-active");
+      }
+    }
+    let btn = document.getElementById("step_btn_" + num);
+    btn.classList.add("is-active");
+
+    var imgs = howto.getElementsByTagName("p");
+    for (var i = 0; i < imgs.length; ++i) {
+      if(!imgs[i].classList.contains("is-hidden")) {
+        !imgs[i].classList.add("is-hidden");
+      }
+    }
+
+    let img = document.getElementById("step_img_" + num);
+    img.classList.remove("is-hidden");
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
