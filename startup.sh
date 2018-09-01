0e8bafa405616d65475efbea811b1aff87ed739d
rballas <rballas62@gmail.com>
2018-09-01 15:25:48 -0400

    Create startup.sh
    Support to server remotely by running this script -- will also run over SSL.

---
+++ startup.sh
@@ -0,0 +1,5 @@
+forever start node_modules/@angular/cli/bin/ng serve \
+    --host www.varietybots.com \
+    --ssl true \
+    --ssl-cert "/var/ssl/varietybots.crt" \
+    --ssl-key "/var/ssl/varietybots.key"
