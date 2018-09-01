2b778d28342f1ab728f806fc784251130e2b9278
rballas <rballas62@gmail.com>
2018-09-01 15:20:43 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+forever start node_modules/@angular/cli/bin/ng serve \
+    --host www.varietybots.com \
+    --disableHostCheck true \
+    --ssl true \
+    --ssl-cert "/var/ssl/varietybots_com.crt" \
+    --ssl-key "/var/ssl/varietybots.key"
